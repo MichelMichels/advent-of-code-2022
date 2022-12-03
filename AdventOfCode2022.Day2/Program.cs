3f1a60a92543e9a6df9a953cf41112e491c0aea5
MichelMichels <michel@miche.ls>
2022-12-03 07:42:06 +0100

    Add ASAP solution

--- AdventOfCode2022.Day2/Program.cs
+++ AdventOfCode2022.Day2/Program.cs
@@ -1,5 +1,7 @@
-﻿
-using AdventOfCode2022.Day2;
+﻿using AdventOfCode2022.Day2;
+using System.Diagnostics;
+
+var sw = Stopwatch.StartNew();
 
 Console.WriteLine("Advent of code 2022 - Day 2");
 Console.WriteLine();
@@ -16,6 +18,13 @@ PlayPartOne(engine, content);
 
 var outcomeEngine = new RockPaperScissorEngine(new OutcomeRockPaperScissorParser());
 PlayPartTwo(outcomeEngine, content);
+
+Console.WriteLine($"Solution found in {sw.ElapsedMilliseconds} ms");
+
+sw = Stopwatch.StartNew();
+PlayPartOneASAP();
+
+Console.WriteLine($"ASAP Solution part one found in {sw.ElapsedMilliseconds} ms");
 
 static void PlayPartOne(IRockPaperScissorEngine engine, string input)
 {
@@ -43,4 +52,20 @@ static void PlayPartTwo(IRockPaperScissorEngine engine, string input)
     Console.ForegroundColor = ConsoleColor.White;
 
     Console.WriteLine();
+}
+
+static void PlayPartOneASAP()
+{
+    var score = File.ReadLines("input.txt")
+        .Select(s => (s[0], s[2]))
+        .Sum(x =>
+        {
+            return (x.Item2 - 'X' + 1) +
+                (x.Item1 == x.Item2 - 23 ? 3 : 0) +
+                ((x.Item1 == 'A' && x.Item2 == 'Y') ? 6 : 0) +
+                ((x.Item1 == 'B' && x.Item2 == 'Z') ? 6 : 0) +
+                ((x.Item1 == 'C' && x.Item2 == 'X') ? 6 : 0);
+        });
+
+    Console.WriteLine(score);
 }
