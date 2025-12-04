a338bd0321bc317c7838c8686099cb89ff74b765
Michel Michels <michel@miche.ls>
2025-12-04 18:37:55 +0100

    Update to .NET 10

---
+++ src/AdventOfCode2022.Day8/Day8Solver.cs
@@ -0,0 +1,8 @@
+﻿using AdventOfCode2022.Shared;
+
+namespace AdventOfCode2022.Day8;
+
+public class Day8Solver(IMessageWriter messageWriter, IInputParser inputParser) : BaseChallengeSolver(messageWriter, inputParser)
+{
+    public override int DayNumber => 8;
+}
---
+++ src/AdventOfCode2022.Day8/Program.cs
@@ -0,0 +1,20 @@
+﻿using AdventOfCode2022.Day8;
+using AdventOfCode2022.Shared;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+        services
+            .AddSingleton<IInputParser, InputParser>()
+            .AddSingleton<IStringSplitter, NewLineSplitter>()
+            .AddSingleton<IConsoleWriter, ConsoleWriter>()
+            .AddSingleton<IMessageWriter, ConsoleMessageWriter>()
+            .AddSingleton<IChallengeSolver, Day8Solver>())
+    .Build();
+
+host.Services
+    .GetRequiredService<IChallengeSolver>()
+    .Solve("input.txt");
+
+await host.RunAsync();
